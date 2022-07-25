4f010fce14411a6b456b0a54e2ea7b4c5576ab26
Jose Luis Estefania <jlrequenaetcheverria@gmail.com>
2022-07-25 12:17:50 +0200

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home } from './componentes/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MyPhotos from './componentes/MyPhotos';
 
 
 
 function App() {
   return (
-    <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="my-photos" element={<MyPhotos/>} />
         </Routes>
-      </BrowserRouter>
-    </div>
   )
 }
 
